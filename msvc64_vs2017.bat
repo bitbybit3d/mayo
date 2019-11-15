d67d2e11dab4c6a825f06964d2e150c30ceaf34e
bitbybit3d <bitbybit3d@163.com>
2019-11-15 16:21:09 +0800

    update used Qt

--- msvc64_vs2017.bat
+++ msvc64_vs2017.bat
@@ -1,8 +1,8 @@
 
-set QTDIR=C:\Qt\Qt5.10.1\5.10.1\msvc2015_64
+set QTDIR=C:\Qt\Qt5.13.1\5.13.1\msvc2017_64
+set PATH=%QTDIR%\bin;%PATH%
 
-set CASROOT=%~d0\sources\contrib\opencascade\opencascade-7.3.0
-
+set CASROOT=%~d0\sources\contrib\opencascade\occt-730-install\x64-Debug
 call %CASROOT%\env.bat vc141 win64 Debug
 
 start "" "%DevEnvDir%\devenv.exe" mayo.sln
